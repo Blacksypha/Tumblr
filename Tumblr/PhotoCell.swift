79497c57111272ef3a85c08ea563e83a90b4dc8e
Cory Dashiell <codas1@morgan.edu>
2018-02-22 04:25:42 -0500

    Create Tumblr-lite

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Tevin Lewis on 2/22/18.
+//  Copyright © 2018 Cory Dashiell. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
